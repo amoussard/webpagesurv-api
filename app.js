7baef8e9e0aae2d458a8b0a3631672b6940da05d
Axel Moussard <axel.moussard@gmail.com>
2017-07-04 16:55:18 -0400

    Add prefix api

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/watch', watch);
+app.use('/api/watch', watch);
 
 // catch 404 and forward to error handler
 // app.use(function (err, req, res, next) {
